3eedb8596bb9dbf36b110c18db9695394bb96904
heyimmatt <mrmcfarland@gmail.com>
2020-07-03 18:12:05 -0700

    Create Player class and let user enter colors for players

--- connect-4-refactor/connect4.js
+++ connect-4-refactor/connect4.js
@@ -6,16 +6,16 @@
  */
 
 class Game {
-  constructor(height = 6, width = 7 ) {
+  constructor(p1, p2, height = 6, width = 7) {
+    this.players = {p1, p2};
     this.height = height;
     this.width = width;
     this.board = [];
-    this.currPlayer = 1;
+    this.currPlayer = p1;
     this.gameOver = false;
     this.makeBoard();
     this.makeHtmlBoard();
   }
-
   /** makeBoard: create in-JS board structure:
    *   board = array of rows, each row is array of cells  (board[y][x])
    */
@@ -68,9 +68,12 @@ class Game {
 
   /** placeInTable: update DOM to place piece into HTML table of board */
   placeInTable(y, x) {
+    console.log(this.players, this.players.p1.color)
     const piece = document.createElement('div');
     piece.classList.add('piece');
-    piece.classList.add(`p${this.currPlayer}`);
+    this.currPlayer === this.players.p1
+      ? (piece.style.backgroundColor = this.players.p1.color)
+      : (piece.style.backgroundColor = this.players.p2.color);
     piece.style.top = -50 * (y + 2);
 
     const spot = document.getElementById(`${y}-${x}`);
@@ -88,44 +91,43 @@ class Game {
       const { board } = this;
       // get x from ID of clicked cell
       const x = +evt.target.id;
-  
+
       // get next spot in column (if none, ignore click)
-      
       const y = this.findSpotForCol(x);
       if (y === null) {
         return;
       }
-  
+
       // place piece in board and add to HTML table
       board[y][x] = this.currPlayer;
       this.placeInTable(y, x);
-  
+
       // check for win
       if (this.checkForWin()) {
         setTimeout(() => {
-          return this.endGame(`Player ${this.currPlayer} won!`);
-        }, 10)
+          return this.endGame(`${this.currPlayer.color} won!`);
+        }, 10);
       }
-  
+
       // check for tie
       if (board.every((row) => row.every((cell) => cell))) {
         this.gameOver = true;
         return this.endGame('Tie!');
       }
-  
+
       // switch players
-      this.currPlayer = this.currPlayer === 1 ? 2 : 1;
+      this.currPlayer = this.currPlayer === this.players.p1 ? this.players.p2 : this.players.p1;
     }
   }
 
   /** checkForWin: check board cell-by-cell for "does a win start here?" */
   checkForWin() {
-    const {height, width, board, currPlayer} = this;
+    const { height, width, board, currPlayer } = this;
     function _win(cells) {
       // Check four cells to see if they're all color of current player
       //  - cells: list of four (y, x) cells
       //  - returns true if all are legal coordinates & all match currPlayer
-  
+
       return cells.every(
         ([y, x]) =>
           y >= 0 &&
@@ -135,7 +137,7 @@ class Game {
           board[y][x] === currPlayer,
       );
     }
-  
+
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         // get "check list" of 4 cells (starting here) for each of the different
@@ -164,7 +166,7 @@ class Game {
           [y + 2, x - 2],
           [y + 3, x - 3],
         ];
-  
+
         // find winner (only checking each win-possibility as needed)
         if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
           this.gameOver = true;
@@ -175,14 +177,20 @@ class Game {
   }
 }
 
+class Player {
+  constructor(color) {
+    this.color = color;
+  }
+}
+
 document.querySelector('button').addEventListener('click', () => {
   if (document.getElementById('column-top')) {
-    if(confirm('Do you want to abandon the game and start over?')) {
+    if (confirm('Do you want to abandon the game and start over?')) {
       location.reload();
     }
-  } else new Game(6, 7)
-})
-
-//To Dos
-// Make it so that you have a button to “start the game” — it should only start the game when this is clicked, and you should be able to click this to restart a new game.
-// Part 3 - player class
+  } else {
+    let p1 = new Player(document.getElementById('p1-color').value);
+    let p2 = new Player(document.getElementById('p2-color').value);
+    new Game(p1, p2, 6, 7);
+  }
+});
